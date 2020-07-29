8355e37cd48375ca8504c9ddc0687a97f0c8f495
Isaac Davis <iqd970@gmail.com>
2020-07-29 13:39:57 -0500

    Update navigation to notifications

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,8 +7,6 @@ import {
   Nav
 } from 'reactstrap';
 
-import Notifications from './Notifications';
-
 export default function Navigation({ isLoggedIn, userRole, handleLogout }) {
   if (isLoggedIn) {
     if (userRole.role === 'admin') {
@@ -67,7 +65,11 @@ function AdminNav({ handleLogout }) {
           <NavLink tag={Link} to="/admin">Admin</NavLink>
         </NavItem>
         <NavItem>
-          <Notifications />
+          <NavLink tag={Link} to="/admin/notifications">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-bell-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+            </svg>
+          </NavLink>
         </NavItem>
       </Nav>
       <Button onClick={handleLogout}>Log Out</Button>
