beccf5974d7c532c3232820fb719b0dc00109111
Isaac Davis <iqd970@gmail.com>
2020-07-29 13:39:33 -0500

    Refactor Notifications as page

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -1,46 +1,41 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import {
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+  Table, Button
 } from 'reactstrap';
 
-import { getNotifications } from '../stitch';
+import { useTimeClockRecords } from './useTimeClockRecords';
 
 export default function Notifications() {
-  const [notifications, setNotifications] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => setIsOpen(isOpen => !isOpen);
-
-  useEffect(() => {
-    (async function fetchNotifications() {
-      const res = await getNotifications();
-      setNotifications(res);
-    })();
-  }, []);
+  const { notifications } = useTimeClockRecords('notifs');
 
   return (
-    <Dropdown isOpen={isOpen} toggle={toggle}>
-      <DropdownToggle caret nav>
-        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-bell-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
-        </svg>
-      </DropdownToggle>
-      <DropdownMenu>
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Date</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
         {notifications.map(notification => {
           notification.prettyDate = moment(notification.date).format('L');
           return (
-            <DropdownItem key={notification._id} tag={Link} to={`/admin/user/${notification.user_id}/${notification.date.valueOf()}`}>
-              <div>{notification.name}</div>
-              <div>{notification.type}</div>
-              <div>{notification.prettyDate}</div>
-            </DropdownItem>
+            <tr key={notification._id}>
+              <td className="align-middle">{notification.name}</td>
+              <td className="align-middle">{notification.type}</td>
+              <td className="align-middle">{notification.prettyDate}</td>
+              <td>
+                <Button tag={Link} to={`/admin/user/${notification.user_id}/${notification.date.valueOf()}`}>View</Button>
+                <Button color="danger" className="ml-1">X</Button>
+              </td>
+            </tr>
           );
         })}
-      </DropdownMenu>
-    </Dropdown>
+      </tbody>
+    </Table>
   );
 }
